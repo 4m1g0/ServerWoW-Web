23eeecfbcb0b8aaf730cffe191b6acf53f717045
Shadez <d.master.me@gmail.com>
2011-12-19 16:16:22 +0900

    wow_user_characters indexes

---
+++ sql/update-19-12-11-2.sql
@@ -0,0 +1,4 @@
+ALTER TABLE  `wow_user_characters` ADD INDEX (  `account` );
+ALTER TABLE  `wow_bugtracker_comments` ADD INDEX (  `report_id` );
+ALTER TABLE  `wow_user_characters` ADD INDEX (  `guid` );
+ALTER TABLE  `wow_user_characters` ADD INDEX (  `realmId` );
