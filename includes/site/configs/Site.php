51e28d7ea45818f4e0ca268b71945a465f74f9e6
4m1g0 <casadelblanco@gmail.com>
2012-04-29 07:10:02 +0200

    Code style in config

--- includes/site/configs/Site.php
+++ includes/site/configs/Site.php
@@ -19,122 +19,122 @@
  **/
 
 $SiteConfig = array (
-  'cache' => 
-  array (
-    'memcached' => 
+    'cache' => 
     array (
-      'enabled' => '0',
-      'configs' => 
-      array (
-        'server' => '127.0.0.1',
-        'port' => '11211',
-      ),
-      'ttl' => '3600',
-	  'nocache' => array(
-		'WowBlogComments' => true, 'WowForumPosts' => true, 'WowForumThreads' => true, 'Characters' => true, 'Account' => true, 'AccountBuyout' => true, 'AccountBanned' => true, 'AccountPoints' => true, 'SmsCodes' => true, 'StoreSession' => true, 'StoreActions' => true, 'IpBanned' => true, 'PaypalHistory' => true, 'Uptime' => true
-	  )
+        'memcached' => 
+        array (
+            'enabled' => '0',
+            'configs' => 
+            array (
+                'server' => '127.0.0.1',
+                'port' => '11211',
+                ),
+            'ttl' => '3600',
+            'nocache' => 
+            array('WowBlogComments' => true, 'WowForumPosts' => true, 'WowForumThreads' => true, 'Characters' => true, 'Account' => true, 'AccountBuyout' => true, 'AccountBanned' => true, 'AccountPoints' => true, 'SmsCodes' => true, 'StoreSession' => true, 'StoreActions' => true, 'IpBanned' => true, 'PaypalHistory' => true, 'Uptime' => true
+            )
+        ),
     ),
-  ),
-  'site' => 
-  array (
-    'path' => '',
-    'creation_youtube_id' => 'DSRrrR4z_ks',
-    'ssl' => '1',
-    'locale_indexes' => 
+    'site' => 
     array (
-      0 => '0',
-      1 => '1',
+        'path' => '',
+        'creation_youtube_id' => 'DSRrrR4z_ks',
+        'ssl' => '1',
+        'locale_indexes' => 
+        array (
+            0 => '0',
+            1 => '1',
+        ),
+        'log' => 
+        array (
+            'enabled' => '1',
+            'debug' => '0',
+            'filename' => 'S:\\home\\wowcs\\www\\webroot\\_debug\\tmp.dbg',
+            'level' => '3',
+            'lag_report' => '0',
+        ),
+        'locale' => 
+        array (
+            'default' => 'es',
+        ),
+        'title' => 'World of Warcraft',
+        'description' => 'Server WoW : El mejor Server de World of Warcraft privado blizzlike Version Cataclysm 4.0.6a y Wrath of The lich king 3.3.5a, Juega Gratis WoW',
+        'keywords' => 'wow, juegos, multijugador masivo, world of warcraft, blizzlike, cataclysm, wotlk, server, privado, foros',
+        'battlegroup' => 'Massive Online',
+        'icons_server' => 'http://eu.battle.net/wow-assets/static/images/icons',
+        'media_server' => 'http://eu.media.blizzard.com',
+        'render_server' => 'http://eu.media.blizzard.com',
     ),
-    'log' => 
+    'misc' => 
     array (
-      'enabled' => '1',
-  	  'debug' => '0',
-      'filename' => 'S:\\home\\wowcs\\www\\webroot\\_debug\\tmp.dbg',
-      'level' => '3',
-	  'lag_report' => '0',
+        'admin_email' => 'admin@wowcs',
     ),
-    'locale' => 
+    'session' => 
     array (
-      'default' => 'es',
+        'identifier' => 'wow_id',
+        'user' => 
+        array (
+            'storage' => 'wow_session',
+        ),
+        'magic_string' => 'SESSION_CONVERT',
     ),
-    'title' => 'World of Warcraft',
-    'description' => 'Server WoW : El mejor Server de World of Warcraft privado blizzlike Version Cataclysm 4.0.6a y Wrath of The lich king 3.3.5a, Juega Gratis WoW',
-    'keywords' => 'wow, juegos, multijugador masivo, world of warcraft, blizzlike, cataclysm, wotlk, server, privado, foros',
-    'battlegroup' => 'Massive Online',
-    'icons_server' => 'http://eu.battle.net/wow-assets/static/images/icons',
-    'media_server' => 'http://eu.media.blizzard.com',
-    'render_server' => 'http://eu.media.blizzard.com',
-  ),
-  'misc' => 
-  array (
-    'admin_email' => 'admin@wowcs',
-  ),
-  'session' => 
-  array (
-    'identifier' => 'wow_id',
-    'user' => 
+    'realms' => 
     array (
-      'storage' => 'wow_session',
+        1 => 
+        array (
+            'id' => '1',
+            'name' => 'Armory Realm',
+            'type' => 'SERVER_TRINITY',
+            'db_id' => 1,
+        ),
     ),
-    'magic_string' => 'SESSION_CONVERT',
-  ),
-  'realms' => 
-  array (
-    1 => 
+    'database' => 
     array (
-      'id' => '1',
-      'name' => 'Armory Realm',
-      'type' => 'SERVER_TRINITY',
-      'db_id' => 1,
+        'realm' => 
+        array (
+            'host' => 'localhost',
+            'user' => 'root',
+            'password' => '',
+            'db_name' => 'trinity_auth',
+            'charset' => 'UTF8',
+            'driver' => 'mysqli',
+            'prefix' => '',
+        ),
+        'characters' => 
+        array (
+            1 => 
+            array (
+                'host' => 'localhost',
+                'user' => 'root',
+                'password' => '',
+                'db_name' => 'trinity_characters',
+                'charset' => 'UTF8',
+                'driver' => 'mysqli',
+                'prefix' => '',
+            ),
+        ),
+        'world' => 
+        array (
+            1 => 
+            array (
+                'host' => 'localhost',
+                'user' => 'root',
+                'password' => '',
+                'db_name' => 'trinity_world',
+                'charset' => 'UTF8',
+                'driver' => 'mysqli',
+                'prefix' => '',
+            ),
+        ),
+        'wow' => 
+        array (
+            'host' => 'localhost',
+            'user' => 'root',
+            'password' => '',
+            'db_name' => 'wow_cs',
+            'charset' => 'UTF8',
+            'driver' => 'mysqli',
+            'prefix' => 'wow',
+        ),
     ),
-  ),
-  'database' => 
-  array (
-    'realm' => 
-    array (
-      'host' => 'localhost',
-      'user' => 'root',
-      'password' => '',
-      'db_name' => 'trinity_auth',
-      'charset' => 'UTF8',
-      'driver' => 'mysqli',
-      'prefix' => '',
-    ),
-    'characters' => 
-    array (
-      1 => 
-      array (
-        'host' => 'localhost',
-        'user' => 'root',
-        'password' => '',
-        'db_name' => 'trinity_characters',
-        'charset' => 'UTF8',
-        'driver' => 'mysqli',
-        'prefix' => '',
-      ),
-    ),
-    'world' => 
-    array (
-      1 => 
-      array (
-        'host' => 'localhost',
-        'user' => 'root',
-        'password' => '',
-        'db_name' => 'trinity_world',
-        'charset' => 'UTF8',
-        'driver' => 'mysqli',
-        'prefix' => '',
-      ),
-    ),
-    'wow' => 
-    array (
-      'host' => 'localhost',
-      'user' => 'root',
-      'password' => '',
-      'db_name' => 'wow_cs',
-      'charset' => 'UTF8',
-      'driver' => 'mysqli',
-      'prefix' => 'wow',
-    ),
-  ),
 );
